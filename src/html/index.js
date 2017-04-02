e564ecce0ecd1c0a1e4fbc1281a346ec3e4eb748
unknown <767030396@qq.com>
2017-04-02 20:45:14 +0800

    'edit'

---
+++ src/html/index.js
@@ -0,0 +1,9 @@
+import "../css/common.less";
+
+document.write("hello world");
+
+//const logoImg = require("../img/logo.png");
+//var img = document.createElement("img");
+//img.src= require("../img/logo.png");
+//document.appendChild(img);
+
